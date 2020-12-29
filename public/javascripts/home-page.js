a808ff2855a371fc9087f8fa3fd68464287632a3
GeorgeII <egorzorikov@gmail.com>
2020-12-29 15:28:51 +0400

    buttons for POST requests

--- public/javascripts/home-page.js
+++ public/javascripts/home-page.js
@@ -1,3 +1,10 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+$('#log-in-button').click(function() {
+  // submit the form
+  $('#log-in-form').submit();
+});
+
+// add an `onclick` handler to the second button
+$('#sign-up-button').click(function() {
+  // submit the form
+  $('#sign-up-form').submit();
+});
